8c55c380c3c14669875cabd0eb23c70db5a2b8e7
JeongPyo <jeongpyo_park@tmax.co.kr>
2020-11-04 15:15:25 +0900

    Class comp 내에서 state 사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello World</h1>
+      <FuncComp></FuncComp>
+      <ClassComp num='3'></ClassComp>
     </div>
   );
+}
+
+function FuncComp() {
+  return(
+    <div className="container">
+      <h2>Func Component</h2>
+    </div>
+    
+  );
+}
+
+class ClassComp extends React.Component {
+  state = {
+    num: this.props.num
+  }
+  render() {
+    return (
+      <div className="container">
+        <h2>Class Component</h2>
+        <p>number: {this.state.num}</p>
+        <input type='button' value='random' onClick={
+          function() {
+            this.setState({
+              num: Math.random()
+            });
+          }.bind(this)
+        }></input>
+      </div>
+    );
+  }
 }
 
 export default App;
